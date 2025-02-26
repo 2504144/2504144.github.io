3624367c769e76bbd015a3dbe3cefc9fd5f0aa33
2504144 <khoi.2008.au@gmail.com>
2025-02-26 14:14:43 -0600

    added the ball

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -5,29 +5,61 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-let rectX;
-let rectY;
-let dy = 5;
+//ball
+let x;
+let y;
+let dx = 2;
+let dy = 2;
+let radius = 25;
+//first player
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  y = height/2;
+  dx = random(-5, 5);
+  dy = random(-5, 5);
 }
 
 function draw() {
-  background('grey');
-  upDownArrows()
-  displayBall()
+  background('light black');
+  displayBall();
+  moveBall();
+  bounce();
+  //upDownArrows();
+}
+
+function moveBall() {
+  x += dx;  
+  y += dy;
+}
+
+function bounce() {
+
+  if (x > width - radius || x < 0 + radius) {
+    dx *= -1;
+  }
+  
+  if (y > height - radius || y < 0 + radius) {
+    dy *= -1;
+  }
 }
 
 function displayBall() {
-  circle(x, y, 50);
+  fill('lime');
+  circle(x, y, radius*2);
+  noStroke();
 }
-function upDownArrows(){
-  if keyIsDown(UP_ARROW){
-    y -= dy;
-  }
-  if keyIsDown(DOWN_ARROW){
-    y += dy;
-  }
+function firstPlayer() {
+  fill('light blue');
+  rect(rectX, rectY, 20, 20);
 }
+
+// function upDownArrows() {
+//   if (keyIsDown(UP_ARROW)) {
+//     y -= dy;
+//   }
+//   if (keyIsDown(DOWN_ARROW)) {
+//     y += dy;
+//   }
+// }
