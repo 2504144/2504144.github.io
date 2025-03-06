78ff52390836218e74c6541b48bdd6703d9cf69f
2504144 <khoi.2008.au@gmail.com>
2025-03-06 14:15:35 -0600

    Update sketch.js

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -15,26 +15,8 @@ function setup() {
 function draw() {
   background(220);
   for (let ball of ballArray){
-    //move ball
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-
-    //teleport to edge of screen
-    if (ball.x > width){
-      ball.x -= width;
-    }
-    else if (ball.x < 0) {
-      ball.x += height;
-    }
-    if (ball.y > height) {
-      ball.y -= height;
-    }
-    if (ball.y < 0) {
-      ball.y += height;
-    }
-    //display ball
-    fill("red");
-    circle(ball.x, ball.y, ball.radius*2);
+    displayBalls(ball);
+    displayBalls(ball);
   }
 }
 
@@ -51,4 +33,31 @@ function spawnBall(){
     dy: random(-5, 5),
   };
   ballArray.push(someBall);
+}
+
+function moveBalls(){
+
+  //move ball
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //teleport to edge of screen
+  if (ball.x - ball.radius > width){
+    ball.x = -ball.radius;
+  }
+  else if (ball.x + ball.radius < 0) {
+    ball.x = width + ball.radius;
+  }
+  if (ball.y - ball.radius > height) {
+    ball.y = -ball.radius;
+  }
+  else if (ball.y + ball.radius < 0) {
+    ball.y = height + ball.radius;
+  }
+}
+
+function displayBalls(){
+//display ball
+  fill("red");
+  circle(ball.x, ball.y, ball.radius*2);
 }
