904c2fe3e66513a4e5ba186d3e0bf6bdbe164d77
2504144 <khoi.2008.au@gmail.com>
2025-03-10 14:11:46 -0600

    added object to assignment

---
+++ arrays-and-object-notation/sketch.js
@@ -0,0 +1,21 @@
+// Maze
+// Khoi Tran
+// March 10th, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  //Object
+  let circle={
+    x: 1,
+    y: 1,
+    radius: 25,
+  };
+}
