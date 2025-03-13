917764153cdf90422ebb4550ff2712684b215530
2504144 <khoi.2008.au@gmail.com>
2025-03-13 12:14:45 -0600

    tried to apply what i learnt from the video and started on making a grid

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -4,22 +4,40 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let list = [];
+let columns, rows;
+let w = 40;
+let grid = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  columns = floor(width/w);
+  rows = floor(height/w);
+
+  for (let j = 0; j < rows; j++){
+    for (let i = 0; i < columns; i++){
+      let cell = new box(i,j);
+    }
+  }
 }
 
 function draw() {
-  background(220);
-  //Object
-  // let circle={
-  //   x: 1,
-  //   y: 1,
-  //   radius: 25,
-  // };
+  background(220); 
+  for (let i = 0; i , boxes.length; i++){
+    boxes[i].show;
+  }
 }
+
+function box(i, j){
+  current.i = i;
+  current.j = j;
+}
+
+current.show= function() {
+  let x = current.i * w;
+  let y = current.j * w;
+  square(x,y,w);
+};
+
 
 
 //recursive back tracking- use this
