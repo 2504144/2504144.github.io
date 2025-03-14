fe62221bdf2d7129aa55a42e38f66b8edfdd1cd2
2504144 <khoi.2008.au@gmail.com>
2025-03-14 14:16:51 -0600

    am working on logic from second video and using surroundings for tile

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -4,41 +4,113 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let columns, rows;
-let w = 40;
-let grid = [];
+let maze;
+let pixelPerTile = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  columns = floor(width/w);
-  rows = floor(height/w);
 
-  for (let j = 0; j < rows; j++){
-    for (let i = 0; i < columns; i++){
-      let cell = new box(i,j);
-    }
-  }
 }
 
 function draw() {
   background(220); 
-  for (let i = 0; i , boxes.length; i++){
-    boxes[i].show;
+  
+}
+
+function makeMaze(w, h){
+  maze = {
+    "stack": [],
+    "tiles": [],
+    "w": w,
+    "h": h,
+  };
+
+  for (let i = 0; i < w; i++){
+    maze.tiles[i] = [];
+    for(let j = 0; j < h; j++){
+      maze.tiles[i][j] = {
+        "up": "wall",
+        "down": "wall",
+        "left": "wall",
+        "right": "wall",
+        "isStart": false,
+        "isCurrent": false,
+        "x": i,
+        "y": j,
+        "seen": false,
+      };
+      if (i === 0 ||  i === w - 1 || j === 0 || j === h - 1){
+        maze.tiles[i][j].seen = true;
+      }
+    }
+  }
+  maze.tiles[1][1].isCurrent = true;
+  maze.tiles[1][1].isStart = true;
+  maze.tiles[1][1].seen = true;
+  maze.stack.push(maze.tiles[1][1]);
+}
+
+function generateTile(){
+  let current = maze.stack.pop();
+  
+  let tileAndWall = chooseNeighbor(current);
+}
+
+function chooseNeighbor(tile){
+  let invis = [];
+
+  let upTile = maze.tiles[tile.x][tile.y + 1];
+  if (!upTile.seen){
+    invis.push(["tile", upTile, "wall", "down"]);
+  }
+
+  let downTile = maze.tiles[tile.x][tile.y - 1];
+  if (!downTile.seen){
+    invis.push(["tile", downTile, "wall", "down"]);
+  }
+
+  let leftTile = maze.tiles[tile.x - 1][tile.y];
+  if (!leftTile.seen){
+    invis.push(["tile", leftTile, "wall", "down"]);
+  }
+
+  let rightTile = maze.tiles[tile.x + 1][tile.y];
+  if (!rightTile.seen){
+    invis.push(["tile", rightTile, "wall", "down"]);
+  }
+
+  if (invis.length === 0){
+    return null;
   }
 }
 
-function box(i, j){
-  current.i = i;
-  current.j = j;
+function oppisiteWall(){
+  if (wall === "up"){
+    return "down";
+  }
+
+  else if (wall === "down"){
+    return "up";
+  }
+
+  else if (wall === "left"){
+    return "right";
+  }
+
+  else if (wall === "right"){
+    return "left";
+  }
 }
 
-current.show= function() {
-  let x = current.i * w;
-  let y = current.j * w;
-  square(x,y,w);
-};
+function drawMaze(){
 
+}
+
+function drawTile(tile, i, j){
+  
+}
 
 
 //recursive back tracking- use this
 //use this link- https://www.youtube.com/watch?v=HyK_Q5rrcr4
+//better link- https://www.youtube.com/watch?v=jQFYh3nRfSQ
