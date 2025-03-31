a734f080131083bc38eb8c7d0e6a12719940d99c
2504144 <khoi.2008.au@gmail.com>
2025-03-31 14:16:36 -0600

    start mine sweeper

---
+++ Projects/grid-based-game/sketch.js
@@ -0,0 +1,51 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// USE THIS AS REFERENCE - https://www.youtube.com/watch?v=LFU5ZlrR21E
+
+let state;
+let bomb;
+const CELL_SIZE = 75;
+let grid;
+let rows;
+let cols;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cols = Math.floor(width/CELL_SIZE);
+  rows = Math.floor(height/CELL_SIZE);
+  grid = generateEmptyGrid(cols, rows);
+}
+
+function draw() {
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+}
